4a37b2c9a8e0cf8eb1816008a48958f4ccf905b6
Gadyll <122042642@upq.edu.mx>
2025-05-19 18:52:19 -0600

    Create EjercicioA.js

---
+++ Practica1/EjercicioA.js
@@ -0,0 +1,13 @@
+//Corrige el siguiente código para que siga las buenas prácticas de JavaScript
+//moderno (usa let y const en lugar de var) y asegúrate de que las variables no se
+//puedan reasignar si no es necesario.
+
+let nombre = "Armando"
+const edad = 25;
+
+nombre = "Ana Maria"
+
+const saludo = "Hola," + nombre + ". Tienes " + edad + " años . ";
+
+console.log(saludo);
+//muestra en consola el contenido de saludo
