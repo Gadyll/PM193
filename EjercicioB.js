129db65ae83a0704fee590dcae16ed2fe26644b2
Gadyll <122042642@upq.edu.mx>
2025-05-19 19:04:22 -0600

    EjercicicoB

---
+++ EjercicioB.js
@@ -0,0 +1,13 @@
+//Convierte esta  afuncion tradicional a una arrow function que haga exactamente lo mismo
+
+//      return numero * numero;
+//  }
+
+//console.log();
+//muestra en consola probando con 3 numeros diferentes
+
+const cuadrado = (numero) => numero * numero;
+
+console.log(cuadrado(45));   
+console.log(cuadrado(19));   
+console.log(cuadrado(76));  
