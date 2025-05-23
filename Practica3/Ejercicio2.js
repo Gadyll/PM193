183555230ec14f2e0a3b1eba7f376dba55172299
Gadyll <122042642@upq.edu.mx>
2025-05-23 14:50:00 -0600

    Ejercicio2

---
+++ Practica3/Ejercicio2.js
@@ -0,0 +1,15 @@
+function verificarUsuario(usuario) {
+  return new Promise((resolve, reject) => {
+    if (usuario === "admin") {
+      resolve("Acceso concedido");
+    } else {
+      reject("Acceso denegado");
+    }
+  });
+}
+
+// admin
+verificarUsuario("llov")
+  .then(res => console.log(res))    
+  .catch(err => console.error(err));
+
