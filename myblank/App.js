ada11c2ea37a63221b4677437b0cde970935e899
Gadyll <122042642@upq.edu.mx>
2025-06-12 16:06:56 -0600

    trabajo 12/06

--- myblank/App.js
+++ myblank/App.js
@@ -1,15 +1,27 @@
+//ZONA1: Importaciones
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
+const Texto=(props)=> {
+  const {contenido}=props
+  return (
+    <Text>{contenido}</Text>
+  )
+}
+//ZONA2: MAIN
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto contenido="Hola"> </Texto>
+      <Texto contenido="Mundo"> </Texto>
+      <Texto contenido="Read Native"> </Texto>
+      <Button title="Presionar"></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//ZONA3: Estilos
 const styles = StyleSheet.create({
   container: {
     flex: 1,
